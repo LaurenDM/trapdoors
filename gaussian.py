cc9b828a64f35a686cbb0e7a85f5ce288a93b392
Kevin King <4kevinking@gmail.com>
2015-11-21 22:43:41 -0500

    pdf lookup table

--- gaussian.py
+++ gaussian.py
@@ -1,4 +1,4 @@
-from math import log, ceil
+from math import log, ceil, floor
 import numpy as np
 import random
 import matplotlib.pyplot as plt
@@ -19,15 +19,27 @@ def test_1D_samp():
     plt.plot(x,y, 'go')
     plt.show()
 
+lookup_tables = {}
+
+def get_lookup_table(sigma, mean, n):
+    def rho(x):
+        return np.exp(-(x - mean)**2 / (2.0 * sigma**2)) / (sigma*np.sqrt(2*np.pi))
+    if (sigma, mean, n) not in lookup_tables:
+        table = {}
+        t = ceil(log(n, 2))
+        for i in range(int(floor(mean-sigma*t)), int(ceil(mean+sigma*t + 1))):
+            table[i] = rho(i)
+        lookup_tables[(sigma, mean, n)] = table
+    return lookup_tables[(sigma, mean, n)]
+
 def gauss_samp_1D(sigma, mean, n):
+    density_table = get_lookup_table(sigma, mean, n)
     sigma = float(sigma)
     mean = float(mean)
     t = ceil(log(n, 2))
-    def rho(x):
-        return np.exp(-(x - mean)**2 / (2.0 * sigma**2)) / (sigma*np.sqrt(2*np.pi))
     while True:
-        x = np.random.randint(mean-sigma*t, mean+sigma*t + 1)
-        gaussian_density = rho(x)
+        x = np.random.randint(int(floor(mean-sigma*t)), int(ceil(mean+sigma*t + 1)))
+        gaussian_density = density_table[x]
         coin_toss = random.uniform(0, 1)
         if coin_toss < gaussian_density:
             return x

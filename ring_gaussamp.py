52a6bdd37d9f9fe43ab3dce05d19fb61b83dcab7
Kevin King <4kevinking@gmail.com>
2015-12-04 14:05:55 -0500

    COV computation

--- ring_gaussamp.py
+++ ring_gaussamp.py
@@ -27,16 +27,40 @@ def ring_sample(sigma, mean, n):
         x.append(gauss_samp_1D(sigma, mean, n))
     return np.matrix(x)
 
-def preimage_sample_A(A_0, R, u, s):
-    print "A_0: " + str(np.shape(A_0))
-    print A_0
+def beta(x):
+    return Rot(x)[1:]
+
+def preimage_sample_A(A, R, E, u, s, q):
+    print "A: " + str(np.shape(A))
+    print A
     print "R:   " + str(np.shape(R))
     w = np.shape(R)[1]
-    (n, mbar) = np.shape(A_0)
+    (n, mbar) = np.shape(A)
     m = w + mbar
     Sigma = s**2
     SigmaG = 2
-    SigmaP = Sigma - np.concatenate([R, np.eye(w)]) * SigmaG * np.concatenate([R.T, np.eye(w)], axis=1)
+    SigmaP = Sigma - R * SigmaG * R.T
+
+    R1 = np.hstack([E[i:] for i in range(E.shape[0])])
+    R2 = np.hstack([R[i:] for i in range(R.shape[0])])
+
+    betaE = np.vstack([beta(E[i:]) for i in range(E.shape[0])])
+    betaR = np.vstack([beta(R[i:]) for i in range(R.shape[0])])
+    COV = np.concatenate(
+            [
+            np.concatenate(
+                [A_mult(q, E, betaE), A_mult(q, E, betaR), R1],
+                axis=1
+                ),
+            np.concatenate(
+                [A_mult(q, R, betaE), A_mult(q, R, betaR), R2],
+                axis=1
+                ),
+            np.concatenate(
+                [R1.T, R2.T, np.eye(R.shape[1])],
+                axis=1
+                ),
+            ], axis=1)
 
     #   perturbation: m = w+mbar ring elements
     p = SigmaP * np.vstack([ring_sample(s, 0, n) for i in range(m)])
